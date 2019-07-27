c02c6bf601dfb5fbd1675f7732af178dc320f085
Al <aliyasharif@hotmail.co.uk>
2019-07-27 14:36:47 +0100

    Added a cloud

--- js/animation.js
+++ js/animation.js
@@ -3,29 +3,36 @@ chuck:{
   xPos:100,
   yPos:100,
   size: 30,
-}
-}
+},
 
+cloud: {
+  xPos:200,
+  yPos:100,
+  size: 50,
+}
+};
 
 
 var canvas = document.querySelector("canvas");
 var ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-
 var plainBackground = document.querySelector("#background");
 ctx.drawImage(plainBackground,0,0,canvas.width,canvas.height);
 console.log();
 
+
 function drawCharacter(){
   var character = document.querySelector("#Chuck");
   ctx.drawImage(character,state.chuck.xPos,state.chuck.yPos,state.chuck.size, state.chuck.size);
-}
+};
+
 drawCharacter();
 
-// function resizeCanvas() {
-//    canvas.width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-//    canvas.height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
-//    WIDTH = canvas.width;
-//    HEIGHT = canvas.height;
-// }
+ function drawCloud() {
+  var cloud = document.querySelector("#Cloud");
+  ctx.drawImage(cloud,state.cloud.xPos,state.cloud.yPos,state.cloud.size, state.cloud.size);
+  console.log("cloud");
+};
+
+drawCloud();
