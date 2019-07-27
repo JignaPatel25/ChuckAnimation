c25fc5c5e078d0c753fa2cfead64dd2c040cbeb2
Zuziii <zuzanapavlatova@gmail.com>
2019-07-27 14:01:22 +0100

    test

--- js/animation.js
+++ js/animation.js
@@ -1,24 +1,13 @@
 var state = {
 chuck:{
- xPos:100,
- yPos:100,
- size: 30,
+  xPos:100,
+  yPos:100,
+  size: 30,
 }
 }
-var canvas = document.querySelector("canvas");
-var ctx = canvas.getContext("2d");
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-var plainBackground = document.querySelector("#background");
-ctx.drawImage(plainBackground,0,0,canvas.width,canvas.height);
-console.log();
-function drawCharacter(){
- var character = document.querySelector("#Chuck");
- ctx.drawImage(character,state.chuck.xPos,state.chuck.yPos,state.chuck.size, state.chuck.size);
-}
-drawCharacter();
-
-
+​
+​
+​
 var canvas = document.querySelector("canvas");
 var ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
