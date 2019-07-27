ac92dd6fe44715e0af0d7a256d241b22710e8eb8
Zuziii <zuzanapavlatova@gmail.com>
2019-07-27 13:50:36 +0100

    Background with bird

---
+++ js/animation.js
@@ -0,0 +1,42 @@
+var state = {
+chuck:{
+ xPos:100,
+ yPos:100,
+ size: 30,
+}
+}
+var canvas = document.querySelector("canvas");
+var ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+var plainBackground = document.querySelector("#background");
+ctx.drawImage(plainBackground,0,0,canvas.width,canvas.height);
+console.log();
+function drawCharacter(){
+ var character = document.querySelector("#Chuck");
+ ctx.drawImage(character,state.chuck.xPos,state.chuck.yPos,state.chuck.size, state.chuck.size);
+}
+drawCharacter();
+
+
+var canvas = document.querySelector("canvas");
+var ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+​
+var plainBackground = document.querySelector("#background");
+ctx.drawImage(plainBackground,0,0,canvas.width,canvas.height);
+console.log();
+​
+function drawCharacter(){
+  var character = document.querySelector("#Chuck");
+  ctx.drawImage(character,state.chuck.xPos,state.chuck.yPos,state.chuck.size, state.chuck.size);
+}
+drawCharacter();
+​
+// function resizeCanvas() {
+//    canvas.width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+//    canvas.height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+//    WIDTH = canvas.width;
+//    HEIGHT = canvas.height;
+// }
