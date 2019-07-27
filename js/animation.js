4f21f81c877a49818fe067a3f63884c1feb16f88
Al <aliyasharif@hotmail.co.uk>
2019-07-27 14:04:17 +0100

    correct version background + bird

--- js/animation.js
+++ js/animation.js
@@ -5,24 +5,24 @@ chuck:{
   size: 30,
 }
 }
-​
-​
-​
+
+
+
 var canvas = document.querySelector("canvas");
 var ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-​
+
 var plainBackground = document.querySelector("#background");
 ctx.drawImage(plainBackground,0,0,canvas.width,canvas.height);
 console.log();
-​
+
 function drawCharacter(){
   var character = document.querySelector("#Chuck");
   ctx.drawImage(character,state.chuck.xPos,state.chuck.yPos,state.chuck.size, state.chuck.size);
 }
 drawCharacter();
-​
+
 // function resizeCanvas() {
 //    canvas.width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 //    canvas.height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
