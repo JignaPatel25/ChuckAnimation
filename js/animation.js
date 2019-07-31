b7f74ece465d38b48481659e4fe73d1cc59d5423
nainabajekal <bajekal.naina@gmail.com>
2019-07-31 18:53:51 +0100

    new javascript with explanations

--- js/animation.js
+++ js/animation.js
@@ -5,14 +5,14 @@ canvas.height = window.innerHeight;
 
 var button1 = {
   x: 300,
-  y: 200,
+  y: 300,
   width: 140,
   height: 70,
 }
 
 var button2 = {
   x: 500,
-  y: 200,
+  y: 300,
   width: 140,
   height: 70,
 }
@@ -30,9 +30,11 @@ gameMode: {
     trueButton: false,
     falseButton: false,
     noClouds: false,
+    answered: false,
   },
-  score:[],
-  runningscore:0,
+score:[],
+runningscore:0
+
 };
 
 // Question page
@@ -41,60 +43,54 @@ var cloud1 = {
   xPos:100,
   yPos:350,
   size:120,
-  // x: Math.floor(Math.random()*(3*canvas.width/10))+(canvas.width/10),
-  // y: Math.floor(Math.random()*(3*canvas.height/10))+(canvas.height/10),
   question1:"When your data is in the cloud",
   question2: "that means it is stored",
   question3: "in satellites in outer space.",
-  correctAnswer: state.buttons[1]
+  correctAnswer: state.buttons[1],
+  explanation: "#explanation1"
 }
 
 var cloud2 = {
   xPos:750,
   yPos:250,
   size:170,
-  // x: Math.floor(Math.random()*(3*canvas.width/10))+(canvas.width/10),
-  // y: Math.floor(Math.random()*(3*canvas.height/10))+(canvas.height/10),
   question1:"Access to the cloud is",
   question2: "usually cheaper than a traditional",
   question3: "desktop computer.",
-  correctAnswer: state.buttons[0]
+  correctAnswer: state.buttons[0],
+  explanation: "#explanation2"
 }
 
 var cloud3 = {
   xPos:100,
   yPos:80,
   size:170,
-  // x: Math.floor(Math.random()*(3*canvas.width/10))+(canvas.width/10),
-  // y: Math.floor(Math.random()*(3*canvas.height/10))+(canvas.height/10),
   question1:"You can access a cloud environment",
   question2: "from anywhere in the world",
   question3: "as you have an Internet connection.",
-  correctAnswer: state.buttons[0]
+  explanation: "#explanation3"
 }
 
 var lightning = {
   xPos:400,
   yPos:280,
   size:200,
-  // x: Math.floor(Math.random()*(3*canvas.width/10))+(canvas.width/10),
-  // y: Math.floor(Math.random()*(3*canvas.height/10))+(canvas.height/10),
   question1:"The cloud is always safer",
   question2: "than local storage.",
   question3:"",
-  correctAnswer: state.buttons[1]
+  correctAnswer: state.buttons[1],
+  explanation: "#explanation4"
 }
 
 var lightning2 = {
   xPos:500,
   yPos:50,
   size:200,
-  // x: Math.floor(Math.random()*(3*canvas.width/10))+(canvas.width/10),
-  // y: Math.floor(Math.random()*(3*canvas.height/10))+(canvas.height/10),
   question1:"It’s more reliable just",
   question2: "to save your documents",
   question3:"directly onto your computer.",
-  correctAnswer: state.buttons[1]
+  correctAnswer: state.buttons[1],
+  explanation: "#explanation5"
 }
 
 state.clouds = [cloud1,cloud2,cloud3,lightning,lightning2]
@@ -135,6 +131,23 @@ function chuckmeetCloud() {
 
       // Button code
 
+      function questionPageScreen() {
+        var plainBackground = document.getElementById("background");
+        ctx.drawImage(plainBackground, 0, 0, canvas.width, canvas.height);
+      }
+
+      function questionPage() {
+        if(state.gameMode.question) {
+        questionPageScreen();
+      	ctx.fillStyle = "black";
+      	ctx.font = "30px Abel";
+      	ctx.fillText(state.touchedCloud.question1, canvas.width/3, canvas.height/5);
+        ctx.fillText(state.touchedCloud.question2, canvas.width/3, 5*canvas.height/20);
+        ctx.fillText(state.touchedCloud.question3, canvas.width/3, 6*canvas.height/20);
+      	trueButton();
+      	falseButton();
+      }}
+
       function trueButton() {
         var button = state.buttons[0];
         ctx.fillStyle = "white";
@@ -155,35 +168,37 @@ function chuckmeetCloud() {
 
       }
 
-      function questionPage() {
-        if(state.gameMode.question) {
-        questionPageScreen();
-      	ctx.fillStyle = "black";
-      	ctx.font = "30px Abel";
-      	ctx.fillText(state.touchedCloud.question1, canvas.width/3, canvas.height/5);
-        ctx.fillText(state.touchedCloud.question2, canvas.width/3, 5*canvas.height/20);
-        ctx.fillText(state.touchedCloud.question3, canvas.width/3, 6*canvas.height/20);
-      	trueButton();
-      	falseButton();
-      }}
+      canvas.addEventListener("click", function(event) {
+        	var sc = document.getElementById("canvas");
+        	var xClick = event.x + window.scrollX;
+        	var yClick = event.y + window.scrollY;
 
-      // function clearScreen() {
-      //   ctx.fillStyle = "white"
-      //   ctx.fillRect(0, 0, canvas.width, canvas.height);
-      // }
+        	for(var i = 0; i < state.buttons.length; i = i + 1){
+        		var button = state.buttons[i];
+        		if (xClick > button.x &&
+        		    xClick < button.x + button.width &&
+        		    yClick > button.y &&
+        		    yClick < button.y + button.height) {
+                  state.gameMode.answered=true;
+                  console.log(state.gameMode.answered)
+            if (button == state.touchedCloud.correctAnswer) {
+                state.runningscore += 10;
+              }
 
-      function questionPageScreen() {
-        var plainBackground = document.getElementById("background");
-        ctx.drawImage(plainBackground, 0, 0, canvas.width, canvas.height);
-      }
+      }}})
+
+      function explanationScreen(){
+        if(state.gameMode.answered){
+          console.log(state.gameMode.answered)
+      var explanations = document.querySelector(state.touchedCloud.explanation);
+      ctx.drawImage(explanations, 0, 0, canvas.width, canvas.height);
+    }}
 
       function drawScore(){
         ctx.fillStyle = "black";
         ctx.font = "italic bold 35pt Abel"; //set the font name and font size
         ctx.fillText("Score:" + state.runningscore, 20, 40);
         var c = document.getElementById("myCanvas");
-
-
       }
 
   function animate() {
@@ -193,6 +208,7 @@ function chuckmeetCloud() {
     chuckmeetCloud();
     questionPage()
     drawScore();
+    explanationScreen();
   }
     animate();
    setInterval(animate, 40);
@@ -217,19 +233,3 @@ window.addEventListener("keydown", function (event) {
         state.chuck.xPos = state.chuck.xPos + 12;
       }
     });
-
-  canvas.addEventListener('click', function(event) {
-  	var sc = document.getElementById("canvas");
-  	var xClick = event.x + window.scrollX - sc.offsetLeft;
-  	var yClick = event.y + window.scrollY - sc.offsetTop;
-
-  	for(var i = 0; i < state.buttons.length; i = i + 1){
-  		var button = state.buttons[i];
-  		if (xClick > button.x &&
-  		    xClick < button.x + button.width &&
-  		    yClick > button.y &&
-  		    yClick < button.y + button.height) {
-
-      if (button == state.touchedCloud.correctAnswer) {
-          state.runningscore += 10;
-        }}}})
